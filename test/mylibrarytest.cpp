e76820641e0a3417578d88e42f274d380791476b
Joaqim Planstedt <git@joaqim.xyz>
2023-01-08 20:22:49 +0100

    add initial test for BalanceDeck

--- test/mylibrarytest.cpp
+++ test/mylibrarytest.cpp
@@ -5,19 +5,26 @@
 #include <catch2/catch.hpp>
 #include <memory>
 
+SCENARIO("BalancedDeck testing", "[test]")
+{
+    GIVEN("can create a BalancedDeck instance")
+    {
 
-SCENARIO("BalancedDeck testing", "[test]") {
-    GIVEN("an Example instance") {
-
-        std::mt19937 generator{
-        std::chrono::system_clock::now().time_since_epoch().count()};
+        std::mt19937 generator{static_cast<long unsigned int>(std::chrono::system_clock::now().time_since_epoch().count())};
 
         auto example = std::make_shared<BalancedDeck>();
 
-        WHEN("calling test()") {
-            auto result = example->rollDie(generator);
-            THEN("result should be an integer [2..12]") {
-                REQUIRE(result >=2 && result <= 12 );
+        WHEN("calling rollDice()")
+        {
+            auto result = example->rollDice(generator);
+            THEN("result should be non-zero")
+            {
+                REQUIRE(result > 0);
+            }
+            THEN("result should be an integer [2..12]")
+            {
+                REQUIRE(result >= 2);
+                REQUIRE(result <= 12);
             }
         }
     }
